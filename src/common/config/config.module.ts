d72130297ea8e0352909206a1da6d4a3c05d4fc2
Ozodbek-cloud <vortextblade16@gmail.com>
2025-07-16 22:36:36 +0500

    Redis System

--- src/common/config/config.module.ts
+++ src/common/config/config.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { MailModule } from '../mail/mail.module';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-    imports: []
+    imports: [JwtModule, MailModule, RedisModule]
 })
 
 export class ConfigModule {}
