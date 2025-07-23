050a325a0d3c4198d1f0b3c012cbca6e7e105711
Ozodbek-cloud <vortextblade16@gmail.com>
2025-07-23 12:09:27 +0500

    .

--- src/modules/homeworks/homeworks.controller.ts
+++ src/modules/homeworks/homeworks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UnsupportedMediaTypeException, UploadedFile, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UnsupportedMediaTypeException, UploadedFile, Query, Req } from '@nestjs/common';
 import { HomeworksService } from './homeworks.service';
 import { CreateHomeworkDto } from './dto/create-homework.dto';
 import { UpdateHomeworkDto } from './dto/update-homework.dto';
@@ -6,8 +6,9 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
 import { v4 as uuidv4 } from "uuid"
-import { ApiConsumes, ApiQuery } from '@nestjs/swagger';
-import { GetHomeworksQueryDto } from './dto/query.dto';
+import { ApiConsumes, ApiOperation, ApiParam, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import { GetHomeworksQueryDto, GetSubmitsQueryDto } from './dto/query.dto';
+import { CheckDto, SubmissionDto } from './dto/submission.dto';
 @Controller('homeworks')
 export class HomeworksController {
   constructor(private readonly homeworksService: HomeworksService) { }
@@ -92,4 +93,47 @@ export class HomeworksController {
   remove(@Param('id') id: string) {
     return this.homeworksService.remove(+id);
   }
+
+  @Get('submission/mine/:lessonId')
+  @ApiOperation({ summary: 'Get my homework submission by lessonId (Student)' })
+  @ApiParam({ name: 'lessonId', type: String })
+  @ApiResponse({ status: 200, description: 'Successfully fetched submission' })
+  getMySubmission(@Param('lessonId') lessonId: string) {
+    return this.homeworksService.get_submissions(lessonId);
+  }
+
+  @Post('submission/submit/:lessonId')
+  @ApiOperation({ summary: 'Submit homework for a lesson (Student)' })
+  @ApiParam({ name: 'lessonId', type: String })
+  @ApiResponse({ status: 201, description: 'Homework submitted successfully' })
+  submitHomework(@Param('lessonId') lessonId: string, @Body() payload: SubmissionDto, @Req() req: Request) {
+    return this.homeworksService.subit_submission(req['user'].id, lessonId, payload);
+  }
+
+  @Get('submissions/all')
+  @ApiOperation({ summary: 'Get all submissions (Mentor, Admin, Assistant)' })
+  @ApiQuery({ name: 'page', required: false })
+  @ApiQuery({ name: 'limit', required: false })
+  @ApiQuery({ name: 'lessonId', required: false })
+  @ApiResponse({ status: 200, description: 'All submissions fetched successfully' })
+  getAllSubmissions(@Query() query: GetSubmitsQueryDto) {
+    return this.homeworksService.get_submits(query);
+  }
+
+  @Get('submissions/single/:id')
+  @ApiOperation({ summary: 'Get single submission by ID (Mentor, Admin, Assistant)' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({ status: 200, description: 'Single submission returned successfully' })
+  getSingleSubmission(@Param('id') id: string) {
+    return this.homeworksService.single(+id);
+  }
+
+  @Post('submission/check')
+  @ApiOperation({ summary: 'Check/Approve/Reject a submission (Mentor, Admin, Assistant)' })
+  @ApiQuery({ name: 'submissionId', type: Number })
+  @ApiResponse({ status: 200, description: 'Submission checked successfully' })
+  checkSubmission(@Query('submissionId') submissionId: string, @Body() payload: CheckDto) {
+    return this.homeworksService.check(+submissionId, payload);
+  }
+
 }
