ce682fba91dfe2880b786a98f03a58fa1ac3ad66
Ozodbek-cloud <vortextblade16@gmail.com>
2025-07-24 15:49:19 +0500

    .

--- src/modules/purchased-courses/purchased-courses.module.ts
+++ src/modules/purchased-courses/purchased-courses.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PurchasedCoursesService } from './purchased-courses.service';
 import { PurchasedCoursesController } from './purchased-courses.controller';
+import { PrismaModule } from 'src/core/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PurchasedCoursesController],
   providers: [PurchasedCoursesService],
 })
