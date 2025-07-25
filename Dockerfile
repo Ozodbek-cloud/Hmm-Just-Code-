9a83a10edc52596c056bd8d603e3cbbaaa1212c5
Ozodbek-cloud <vortextblade16@gmail.com>
2025-07-25 18:44:55 +0500

    Last Stand,

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ COPY . .
 EXPOSE 4077
 
 RUN npx prisma generate
-npx prisma migrate deploy
+RUN npx prisma migrate deploy
 RUN npm run build
 
 CMD ["npm", "run", "start:dev"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from './common/config/config.module';
-import { ContactModule } from './modules/contact/contact.module';
 
 @Module({
   imports: [ConfigModule],
