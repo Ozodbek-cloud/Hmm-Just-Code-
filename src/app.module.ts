150329be4a91fb324ba6fd6e2c5fa9c8dbb4335b
Ozodbek-cloud <vortextblade16@gmail.com>
2025-07-25 22:58:45 +0500

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from './common/config/config.module';
+import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
 @Module({
-  imports: [ServeStaticModule.forRoot({
-    rootPath: join(__dirname, '..', 'uploads'), 
-    serveRoot: '/uploads', 
-  }),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
     ConfigModule],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './common/config/swagger.set-up';
 import * as dotenv from "dotenv"
 
+dotenv.config();
 
-dotenv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
-  }))
+  }));
 
-  await setupSwagger(app)
-  await app.listen(process.env.PORT ?? 4000, () => console.log(`Running on ${process.env.PORT}`));
+  await setupSwagger(app);
+
+  await app.listen(process.env.PORT ?? 4000, '0.0.0.0', () =>
+    console.log(`Running on ${process.env.PORT}`)
+  );
 }
-
+bootstrap();
